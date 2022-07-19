cd81cedb0f7fdf5aa20258fd9736eb09ea891ea4
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-07-18 19:24:57 -0600

    doing ARRAYS

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var person = {
     name: 'Zach',
     age: 22,
-    nickname: 'Z'
+    nickname: 'Z',
+    hobbies: ['Sports', 'cooking', 'gaming']
 };
+var favoriteAct;
+favoriteAct = ['sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
 console.log(person.nickname);
--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
 const person = {
   name: 'Zach',
   age: 22,
-  nickname: 'Z'
+  nickname: 'Z',
+  hobbies: ['Sports', 'cooking', 'gaming']
+}
+
+let favoriteAct: string[];
+favoriteAct = ['sports'];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
 }
 
 console.log(person.nickname)
